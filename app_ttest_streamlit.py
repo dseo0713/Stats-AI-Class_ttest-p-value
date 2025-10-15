f789f33cdb4303a353d5f5b76b0bcff4f4267c66
dseo0713 <david_seo@stud.assist.ac.kr>
2025-10-15 13:49:15 +0900

    Update app_ttest_streamlit.py

--- app_ttest_streamlit.py
+++ app_ttest_streamlit.py
@@ -75,8 +75,7 @@ st.markdown("""
 <style>
 /* 사이드바 내의 모든 버튼을 타겟팅합니다. */
 [data-testid="stSidebar"] button {
-    /* 버튼을 정사각형 (예: 30px x 30px)으로 만들고 !important로 기본 스타일을 강제 오버라이드 */
-    width: 30px !important;
+    width: 35px !important;
     height: 3px !important;
     /* 버튼 내부의 텍스트/기호를 중앙에 배치하기 위해 padding을 제거합니다. */
     padding: 0;
